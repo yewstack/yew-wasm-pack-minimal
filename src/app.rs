2cdf5567974b789273a87e42a5e7ba9828b37cc4
Kelly Thomas Kline <kellytk@sw-e.org>
2019-08-11 21:21:37 -0700

    Create app.rs

---
+++ src/app.rs
@@ -0,0 +1,26 @@
+use yew::prelude::*;
+
+pub struct App {}
+
+pub enum Msg {}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        App {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+}
+
+impl Renderable<App> for App {
+    fn view(&self) -> Html<Self> {
+        html! {
+            <p>{ "Hello world!" }</p>
+        }
+    }
+}
