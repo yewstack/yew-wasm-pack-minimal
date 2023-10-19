5f24fb95765300d8442379d0098c7fd27adab7b8
wiseaidev <oss@wiseai.dev>
2023-10-19 11:15:20 +0300

    bump wasm-bindgen, yew, rust, migrate from a struct based component to a functional component

--- src/app.rs
+++ src/app.rs
@@ -1,28 +1,8 @@
 use yew::prelude::*;
 
-pub struct App {}
-
-pub enum Msg {}
-
-impl Component for App {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        App {}
-    }
-
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        true
-    }
-
-    fn change(&mut self, _: Self::Properties) -> ShouldRender {
-        false
-    }
-
-    fn view(&self) -> Html {
-        html! {
-            <p>{ "Hello world!" }</p>
-        }
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <p>{ "Hello world!" }</p>
     }
 }
